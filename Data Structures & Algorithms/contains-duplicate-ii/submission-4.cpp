3590a6c88e832178ed93de169032581d599c58b8
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-07-03 22:28:26 +0530

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        while(right<n){
+            if(nums[left] == nums[right] && abs(left-right)<=k){
+                return true;
+            }
+            left++;
+        }
+        return false;
+
+        
+    }
+};
