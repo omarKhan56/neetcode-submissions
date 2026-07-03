2672629c1ecc06fff6012ac2529cb88f5b3fd6b6
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-07-03 22:54:51 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxprofit = 0;
+        int i = 0;
+        int j = 1;
+        while (j < n) {
+            if (prices[j] > prices[i]) {
+                maxprofit = max(maxprofit, prices[j] - prices[i]);
+            }
+            else{
+                i = j;
+            }
+            j++;
+
+        }
+        return maxprofit;
+    }
+};
