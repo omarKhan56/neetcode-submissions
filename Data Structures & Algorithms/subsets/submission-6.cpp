1b6b6fa345593eb7f887a3673035b5f984513ecc
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-07-14 23:44:33 +0530

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    int n;
+    vector<vector<int>> result;
+    void solve(int idx, vector<int>& nums, vector<int>& temp) {
+        result.push_back(temp);
+        if (idx == n) {
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            temp.push_back(nums[i]);
+            solve(i + 1, nums, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        n = nums.size();
+        vector<int> temp;
+        solve(0, nums, temp);
+        return result;
+    }
+};
