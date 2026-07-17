91a83df244bc0213d6634e28735b756fb7d1e718
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-07-17 23:00:14 +0530

    Add: permutations-ii - submission-10

---
+++ Data Structures & Algorithms/permutations-ii/submission-10.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    int n;
+    vector<vector<int>> result;
+    bool isPresent(vector<int>& temp) {
+        for (int i = 0; i < result.size(); i++) {
+            if (result[i] == temp) {
+                return true;
+            }
+        }
+        return false;
+    }
+    void solve(int idx, vector<int>& nums) {
+        if (idx == n) {
+            if (!isPresent(nums)) {
+                result.push_back(nums);
+                return;
+            }
+        }
+        for (int i = idx; i < n; i++) {
+            swap(nums[idx], nums[i]);
+            solve(idx + 1, temp);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
+        solve(0, nums);
+        return result;
+    }
+};
