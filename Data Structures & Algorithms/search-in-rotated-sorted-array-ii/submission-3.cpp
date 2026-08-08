1ac34f2531c5852af3f3f775df4c43fd3a125331
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-08-08 18:26:38 +0530

    Add: search-in-rotated-sorted-array-ii - submission-3

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-3.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int FindPivot(vector<int>& nums, int l, int r) {
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+            else if (nums[mid] == nums[r]) {
+                r = r - 1;
+            }
+            else {
+                r = mid;
+            }
+        }
+
+        return r;
+    }
+
+    int BinarySearch(vector<int>& nums, int l, int r, int target) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            else if (nums[mid] < target)
+                l = mid + 1;
+
+            else
+                r = mid - 1;
+        }
+
+        return -1;
+    }
+
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int pivot = FindPivot(nums, 0, n - 1);
+
+        if (nums[pivot] == target) {
+            return true;
+        }
+
+        int idx = BinarySearch(nums, pivot + 1, n - 1, target);
+
+        if (idx != -1) {
+            return true;
+        }
+
+        idx = BinarySearch(nums, 0, pivot - 1, target);
+
+        if (idx != -1) {
+            return true;
+        }
+
+        return true;
+    }
+};
