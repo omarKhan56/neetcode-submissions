5b17164c8391def5e94979f168adaceaa7b79fd3
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-09-01 17:30:40 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+
+        for (string op : operations) {
+            if (op != "+" && op != "D" && op != "C") {
+                st.push(stoi(op));
+            } else if (op == "+") {
+                int first = st.top();
+                st.pop();
+
+                int second = st.top();
+
+                int sum = first + second;
+
+                st.push(first);
+                st.push(sum);
+            } else if (op == "D") {
+                st.push(2 * st.top());
+            } else if (op == "C") {
+                st.pop();
+            }
+        }
+
+        int total = 0;
+
+        while (!st.empty()) {
+            total += st.top();
+            st.pop();
+        }
+
+        return total;
+    }
+};
