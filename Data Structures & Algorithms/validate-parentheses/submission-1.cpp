e667fa2bde33d0823697c952f37c22d4719cfee1
Omar Ali Khan <omaralikhan3356@gmail.com>
2026-09-01 17:05:26 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<int> st;
+        for (char ch : s) {
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s[i];
+
+                if (ch == '(' || ch == '[' || ch == '{') {
+                    st.push(ch);
+                } else {
+                    if (st.empty()) {
+                        return false;
+                    }
+                }
+                int top = st.top();
+                if (top == '(' && ch == ')')
+                    || (top == '[' && ch == ']') || (top == '{' && ch == '}') { st.pop(); }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
